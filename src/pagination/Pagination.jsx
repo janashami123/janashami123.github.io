489ed7bcc014e12e430a5213194369f61c330e12
janashami123 <106778129+janashami123@users.noreply.github.com>
2022-12-23 10:28:10 +0200

    edits

--- src/pagination/Pagination.jsx
+++ src/pagination/Pagination.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Pagination.css'
 function Pagination({booksPerPage,totalBooks,paginate}) {
-    const bookNumbers=[];
+    const pageNumbers=[];
     for(let i = 1;i <= Math.ceil(totalBooks / booksPerPage);i++){
-        bookNumbers.push(i);
+        pageNumbers.push(i);
     }
   return (
     <nav>
    <ul className='pagination'>
-    {bookNumbers.map(number=>
+    {pageNumbers.map(number=>
       (<li key={number} className='page-item'>
         <a onClick={()=>paginate(number)}
         href='#' className='page-link'>
